ac94e70c7b6327ee4b5f61bcdc89619a3f2a3bea
Nancy Saad <Nancy.Saad9583@compit.aun.edu.eg>
2025-07-05 19:03:40 +0300

    Add project files.

---
+++ CarpetsCleaningServiceTask/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace CarpetsCleaningServiceTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of small carpets: ");
+            int smallCarpets = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number of large carpets: ");
+            int largeCarpets = Convert.ToInt32(Console.ReadLine());
+            int cost = (smallCarpets * 25) + (largeCarpets * 35);
+            double tax = cost * 0.06;
+            Console.WriteLine("Price per small room: $25");
+            Console.WriteLine("Price per large room: $35");
+            Console.WriteLine($"Cost: {cost:C}");
+            Console.WriteLine($"Taxes: {tax:C}");
+            Console.WriteLine("===============================");
+            Console.WriteLine($"Total: {(cost + tax):C}");
+            Console.WriteLine("This estimate is valid for 30 days");
+
+
+        }
+    }
+}
---
+++ FirstProject/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace FirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter your favorite number between 1 and 100");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"No really !! , {num} is my favorite number! ");
+        }
+    }
+}
